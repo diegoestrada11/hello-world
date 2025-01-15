0955d96eb93f768a74ca5f650a697fa708abd530
diegoestrada11 <est21016@byui.edu>
2025-01-14 18:18:49 -0700

    changes

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+# Hello World in Python.
+
+# Ask the user for their name and print a greeting to them.
+user_name = input("What is your name? ")
+
+# Print a greeting to the user.
+print("Hello " + user_name)
